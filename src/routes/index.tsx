33fccdc42565ef5fb6f1240a9098ff20d5c38a04
André Dourado <andrew.dourado.freitas@gmail.com>
2020-11-13 14:49:08 -0300

    Initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" />
+  </Switch>
+);
+
+export default Routes;
---
+++ src/styles/global.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    outline: 0;
+  }
+`;
