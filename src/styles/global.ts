de739c20a3b603252eefae433f0be2abd3ac1e12
André Dourado <andrew.dourado.freitas@gmail.com>
2020-11-14 10:35:39 -0300

    style: ajust global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,4 +7,20 @@ export default createGlobalStyle`
     padding: 0;
     outline: 0;
   }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    background: #2a2d3a;
+    font-family: 'Poppins', sans-serif;
+    color: #fff;
+  }
+
+  button {
+    background: none;
+    border: 0;
+    color: #fff;
+  }
 `;
